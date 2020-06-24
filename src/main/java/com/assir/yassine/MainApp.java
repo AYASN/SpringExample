bef1db4af9822c0c4f4091aa1b4702f58e48b155
AYassine <assiryassine@gmail.com>
2020-06-24 07:30:13 +0200

    Bean life cycle management using Bean Post Processor

--- src/main/java/com/assir/yassine/MainApp.java
+++ src/main/java/com/assir/yassine/MainApp.java
@@ -1,4 +1,14 @@
 package com.assir.yassine;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainApp {
+    public static void main(String[] args) {
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
+        context.registerShutdownHook();
+    }
 }
